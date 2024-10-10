35019642f1284a295b014855855aa658404078ef
Denrik <ketovdany@yandex.ru>
2024-10-10 18:33:52 +0400

    дз10

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        String phone = "9179448478";
-        if (phone.length() == 10) {
-            phone = "+7" + phone;
-            System.out.println("phone = " + phone);
-        } else if (phone.length() > 10) {
-            System.out.println("Телефон слишком длинный");
-        } else if (phone.length() < 10) {
-        System.out.println("Телефон слишком короткий");
-
+        //Задание 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        //Задание 2
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        //Задание 3
+        fullName = "Иванов Семён Семёнович";
+        for (int i = 0; i < fullName.length(); i++) {
+            if (fullName.charAt(i) != 'ё' && fullName.charAt(i) != 'Ё') {
+                fullName = fullName.replace("ё", "е");
+                fullName = fullName.replace("Ё", "Е");
+            }
         }
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
-}
+        }
+
+
+
+
